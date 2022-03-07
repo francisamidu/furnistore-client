fb85eee975a9d65ea95f04affad5b87e684d0e64
francisamidu <francisamidu124@gmail.com>
2022-03-07 04:10:07 +0200

    Deleted VerifyOTP

--- pages/verify-otp.tsx
+++ pages/verify-otp.tsx
@@ -1,10 +1,41 @@
+import React, { FormEvent, useState } from "react";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import router from "next/router";
 import { NextComponentType } from "next";
-import React from "react";
-import { AuthLayout } from "../components";
-import VerifyOTP from "../components/VerifyOTP";
+import { AuthLayout, Button } from "../components";
 
 const Verification = () => {
-  return <VerifyOTP />;
+  const [otp, setOTP] = useState("");
+
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //Attach otp to request and send to backend
+  };
+  return (
+    <form
+      className="col-start-1 md:col-end-4 col-end-5"
+      onSubmit={submitHandler}
+    >
+      <h1 className="text-3xl font-bold mb-4">Welcome, Verify your email</h1>
+      <h2 className="text-1xl text-gray-500 mb-8">
+        Retrieve the OTP sent to your email and enter it below
+      </h2>
+      <div className="input-group my-4 w-full">
+        <input
+          type="text"
+          placeholder="Enter OTP"
+          className="rounded-sm py-2 px-2 border-2 border-gray-200 w-10/12"
+          onChange={(event) => setOTP(event.target.value)}
+        />
+      </div>
+      <div className="flex flex-row items-center">
+        <Button
+          text="Continue"
+          icon={<AiOutlineArrowRight className="text-white" />}
+        />
+      </div>
+    </form>
+  );
 };
 Verification.getLayout = (page: NextComponentType) => {
   return (
